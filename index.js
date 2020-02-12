41328bc97c4f5ae8cf8dfeb1de75821dd3ec602e
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-12 13:31:23 -0300

    set up index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const blogRouter = require('./routers/blogRouter');
+
+const server = express();
+
+server.use(express.json());
+
+// server.use('/api/posts', blogRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h2>Lambda Blog API</h>
+    <p>Welcome to the Lambda Blog API</p>
+  `);
+})
+
+const port = 5000;
+server.listen(port, () => {
+    console.log(`\n*** Server running on http://localhost:${port} ***\n`);
+});
